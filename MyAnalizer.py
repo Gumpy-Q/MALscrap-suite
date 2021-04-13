8c41ec5a32bed70b93df3996201d8b543d1dd123
Gumpy-Q <qgumpert@gmail.com>
2021-04-13 08:57:06 +0200

    Update MyAnalizer.py

--- MyAnalizer.py
+++ MyAnalizer.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import matplotlib.ticker as ticker
 from matplotlib.patches import Patch
+from matplotlib.ticker import PercentFormatter
+from matplotlib.ticker import MaxNLocator
 
 import seaborn as sb
 
@@ -31,7 +33,7 @@ enlarge_fig=(15,10)
 picked_colors=['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9', '#ffffff', '#000000']
 
 
-def stackbarcolor(df_plot,cat_list,ax,plot_name,colors_list,cat_key,tosum_key,ylabel_name,ymax=0):
+def stackbarcolor(df_plot,cat_list,ax,plot_name,colors_list,cat_key,tosum_key,ylabel_name,max_year,min_year,ymax=0):
     df_plot['bottom']=0    
     
     for cat_value,color in zip(cat_list,colors_list): #I want to attribute a color for each source that will be consistent for each type of anime
@@ -50,28 +52,29 @@ def stackbarcolor(df_plot,cat_list,ax,plot_name,colors_list,cat_key,tosum_key,yl
             ax.axis(xmax=df_plot['release-year'].max()+1,xmin=df_plot['release-year'].min()-1)
             ax.tick_params('x',labelrotation=45, labelsize=font)
             ax.tick_params('y', labelsize=font)
+            ax.set(xlim=(min_year-1,max_year+1))
             ax.ticklabel_format(axis='x', style='plain', useOffset=False) #If I don't do this plt want to put the label to engineering notation
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(base=round((df_plot['release-year'].max()-df_plot['release-year'].min())/5))) #I want only 5 step for xlabel
-    
+            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+            
     ymax=max(df_plot['bottom'].max(),ymax) #after each season I retrieve the maximum value to limit plot axis
     
     return ymax
 
-def production(df,min_year,max_year,anitypes,color_list): #To vizualize the sum of anime product each year for each season
+def production_month(df,min_year,max_year,anitypes,color_list): #To vizualize the sum of anime product each year for each season
     
     season_analyze=df.value_counts(['release-year','release-season','type']).reset_index(name='count') #count occurence and build the dataframe with a new column 'count'
 
     select_years=season_analyze[(season_analyze['release-year']<=max_year) & (season_analyze['release-year']>=min_year)] #remove years out of study scope
     select_years=select_years[select_years['type'].isin(anitypes)]
     
+    picked_colors=color_list[0:len(anitypes)+1]
+    
+    custom_patches=[]
+
     fig, axes = plt.subplots(2,2,figsize=enlarge_fig) #building a subplot for the 4 seasons
     axes = axes.flatten()
     
     ymax=0
-    
-    picked_colors=color_list[0:len(anitypes)+1]
-    
-    custom_patches=[]
     
     #avoid colors mismatch when getting legend
     for color in picked_colors:
@@ -82,10 +85,41 @@ def production(df,min_year,max_year,anitypes,color_list): #To vizualize the sum 
 
         # print('--------------'+season)
         
-        ymax=stackbarcolor(df_season,anitypes,ax,season,picked_colors,'type','count','Count',ymax)
+        ymax=stackbarcolor(df_season,anitypes,ax,season,picked_colors,'type','count','Count',max_year,min_year,ymax)
         
     for ax in axes:
         ax.axis(ymax=ymax+5) #And then I set the limit
+        ax.ticklabel_format(axis='x', style='plain', useOffset=False)
+        
+    fig.suptitle('Evolution of the production',fontsize=font)
+    fig.tight_layout()
+    fig.legend(custom_patches, anitypes, bbox_to_anchor=(1,0.6), loc="upper left",fontsize=font)
+    return fig
+
+def production_year(df,min_year,max_year,anitypes,color_list): #To vizualize the sum of anime product each year for each season
+    
+    season_analyze=df.value_counts(['release-year','type']).reset_index(name='count') #count occurence and build the dataframe with a new column 'count'
+
+    select_years=season_analyze[(season_analyze['release-year']<=max_year) & (season_analyze['release-year']>=min_year)] #remove years out of study scope
+    select_years=select_years[select_years['type'].isin(anitypes)]
+    
+    picked_colors=color_list[0:len(anitypes)+1]  
+    custom_patches=[]    
+    #avoid colors mismatch when getting legend
+    for color in picked_colors:
+        custom_patches.append(Patch(facecolor=color, edgecolor='b'))
+        
+    fig, ax = plt.subplots(1,figsize=enlarge_fig) #building a subplot for the 4 seasons
+    
+    ymax=0
+     
+    df_year=select_years.sort_values('release-year') #reducing the DataFrame to the season studied
+
+        # print('--------------'+season)
+        
+    ymax=stackbarcolor(df_year,anitypes,ax,'',picked_colors,'type','count','Count',max_year,min_year,ymax)
+        
+    ax.axis(ymax=ymax+5) #And then I set the limit
     
     fig.suptitle('Evolution of the production',fontsize=font)
     fig.tight_layout()
@@ -105,7 +139,7 @@ def episode(df,min_year,max_year,anitype,max_shown): #This function is showing t
     ax.set(ylim=(0,max_shown))
     ax.xaxis.set_major_locator(ticker.MultipleLocator(base=round((max_year-min_year)/10))) # I want to limit the number of label shown
     ax.set_title('Repartion of anime length : '+ anitype,fontsize=font)
-    
+        
     fig.tight_layout()
     return fig    
 
@@ -147,9 +181,10 @@ def source(df,min_year,max_year,anitypes,color_list,thresold):
             df_type=select_years[select_years['type']==anime_type].sort_values('release-year') #reducing the DataFrame to the season studied I need the year to be at the right order for the stacking
             # print('--------------'+anime_type)
             
-            stackbarcolor(df_type,sources,ax,anime_type,picked_colors,'source-material','percent','part of the diffusion')
+            stackbarcolor(df_type,sources,ax,anime_type,picked_colors,'source-material','percent','part of the diffusion',max_year,min_year)
             for ax in axes:
-                ax.set(ylim=(0,1))        
+                ax.set(ylim=(0,1))
+                ax.yaxis.set_major_formatter(PercentFormatter(xmax=1, decimals=0, symbol='%', is_latex=False))
                 
     else:
         fig, ax = plt.subplots(1,1,figsize=enlarge_fig) #building a subplot for the one choosen
@@ -157,8 +192,8 @@ def source(df,min_year,max_year,anitypes,color_list,thresold):
         anime_type=anitypes[0]
         # print('--------------'+anime_type)
           
-        stackbarcolor(df_type,sources,ax,anime_type,picked_colors,'source-material','percent','part of the diffusion')
-        
+        stackbarcolor(df_type,sources,ax,anime_type,picked_colors,'source-material','percent','part of the diffusion',max_year,min_year)
+        ax.set(ylim=(0,1))
     
     fig.suptitle('Source of the adaptation (if less than '+str(thresold)+'% -> Other)',fontsize=font)          
     fig.tight_layout()
@@ -210,8 +245,11 @@ while datavalid==False:
        print(anime_types)
 
 
-fig_prod=production(raw,start_year,end_year,type_to_viz,picked_colors)
-fig_prod.show()
+fig_prod_m=production_month(raw,start_year,end_year,type_to_viz,picked_colors)
+fig_prod_m.show()
+
+fig_prod_y=production_year(raw,start_year,end_year,type_to_viz,picked_colors)
+fig_prod_y.show()
 
 fig_source=source(raw,start_year,end_year,type_to_viz,picked_colors,2.5)
 fig_source.show()
@@ -222,4 +260,4 @@ fig_ep.show()
 fig_ep=episode(raw,start_year,end_year,'TV (Continuing)',150)
 fig_ep.show()
 
-input('press key') 
+input('press Enter key') 
